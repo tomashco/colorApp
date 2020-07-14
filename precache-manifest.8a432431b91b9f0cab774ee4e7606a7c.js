b68b5d7ea270f139767944042e26bb78b8e84d8e
Tommaso Maioli <tommaso.maioli@gmail.com>
2020-07-14 16:30:08 +0200

    Updates

---
+++ precache-manifest.8a432431b91b9f0cab774ee4e7606a7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a0b6478391d87d848dff6dd92ee9d0",
+    "url": "/tomashco/colorApp/index.html"
+  },
+  {
+    "revision": "f511a2e8ddf6cba1e1cc",
+    "url": "/tomashco/colorApp/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "f28e16db85f8993d743c",
+    "url": "/tomashco/colorApp/static/css/main.2f54813b.chunk.css"
+  },
+  {
+    "revision": "f511a2e8ddf6cba1e1cc",
+    "url": "/tomashco/colorApp/static/js/2.3ca62ca3.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/tomashco/colorApp/static/js/2.3ca62ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28e16db85f8993d743c",
+    "url": "/tomashco/colorApp/static/js/main.06571fd2.chunk.js"
+  },
+  {
+    "revision": "bc843c769cd5510dbcdb",
+    "url": "/tomashco/colorApp/static/js/runtime-main.918e86b9.js"
+  },
+  {
+    "revision": "3c839eb1112e6c212d74678767e20aa0",
+    "url": "/tomashco/colorApp/static/media/bg.3c839eb1.svg"
+  }
+]);
